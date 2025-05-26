3a9fc8da2a352d5142ee0ca14b0410719047cb2f
hoang cuong <vancuong25112003@gmail.com>
2025-05-27 01:14:11 +0700

    home page ui

--- frontend/src/app/(guest)/Home.tsx
+++ frontend/src/app/(guest)/Home.tsx
@@ -1,8 +1,59 @@
-import React from 'react'
+'use client'
+
+import { AppContext } from '@/context/AppContext'
+import { Heart } from 'lucide-react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import React, { useContext } from 'react'
 
 const Home = () => {
+
+    const { listings } = useContext(AppContext)
+
+    const router = useRouter()
+
     return (
-        <div>Home</div>
+        <div className="p-4">
+            <h2 className="text-xl font-semibold mb-4">
+                Nơi lưu trú được ưa chuộng tại Hồ Chí Minh
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-x-auto">
+                {listings.map((listing, index) => (
+                    <div className="flex flex-col" key={index}>
+                        <div key={index} className="relative">
+                            <div className="relative h-56 w-full">
+                                <Image
+                                    src={typeof listing.images[0] === "string"
+                                        ? listing.images[0]
+                                        : (listing.images[0]?.url || "/placeholder.png")}
+                                    alt={listing.title}
+                                    fill
+                                    className="object-cover rounded-lg"
+                                    onClick={() => router.push(`/room/${listing.id}`)}
+                                />
+                                <button className="absolute top-2 right-2 bg-white rounded-full p-1">
+                                    <Heart
+                                        className={`w-5 h-5 ${true ? "fill-red-500 text-red-500" : "text-gray-500"
+                                            }`}
+                                    />
+                                </button>
+                                {true && (
+                                    <span className="absolute top-2 left-2 bg-white text-xs px-2 py-1 rounded">
+                                        Được khách yêu thích
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+                        <div className="p-2">
+                            <p className="text-sm font-semibold">{listing.title}</p>
+                            <p className="text-sm text-muted-foreground">
+                                ₫{listing.pricePerNight} cho 2 đêm • ⭐ 5   (100 đánh giá)
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
