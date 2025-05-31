d6c83c4626ca6b6ccf23976655c96a7e125a5e44
hoang cuong <vancuong25112003@gmail.com>
2025-05-31 12:58:18 +0700

    add to wishlist

---
+++ backend/src/modules/wishlist/wishlist.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
+import { WishlistService } from './wishlist.service';
+import { CreateWishlistDto } from './dto/create-wishlist.dto';
+import { UpdateWishlistDto } from './dto/update-wishlist.dto';
+
+@Controller('wishlist')
+export class WishlistController {
+  constructor(private readonly wishlistService: WishlistService) { }
+
+  @Post('add-to-wishlist')
+  addToWishlist(
+    @Body() listingId: string,
+    @Req() req
+  ) {
+    return this.wishlistService.addToWishlist(+listingId, req.user.id);
+  }
+
+  @Get()
+  findAll() {
+    return this.wishlistService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.wishlistService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateWishlistDto: UpdateWishlistDto) {
+    return this.wishlistService.update(+id, updateWishlistDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.wishlistService.remove(+id);
+  }
+}
