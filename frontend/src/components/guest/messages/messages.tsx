319f756a398f18f92e36e19ae54d827fc120c83b
huysadboizdev <haquanghuydz1996@gmail.com>
2025-06-02 18:54:54 +0700

    ib

---
+++ frontend/src/components/guest/messages/messages.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SidebarInbox from '../../user/SidebarInbox'
+
+const Messages = () => {
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/4">
+        <SidebarInbox />
+      </div>
+      <div className="flex-1 bg-gray-100">
+       
+        <div className="flex items-center justify-center h-full text-gray-500">
+          Select a message to view
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Messages
